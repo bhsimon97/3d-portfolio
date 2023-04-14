4fe10fa011ad5f44e698485e92b710c7a336b43d
bhsimon97 <76971018+bhsimon97@users.noreply.github.com>
2023-04-14 13:05:34 -0500

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "Test Engineer",
+    icon: mobile,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer II - QA",
+    company_name: "Infosys",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "October 2022 - Present",
+    points: [
+      "Prevent defects from reaching production via development & debugging of 200+ automated test cases",
+      "Ensure compliance to security requirements by implementing and maintaining Hitachi PAMUtil integration with all automated test cases",
+      "Lead 3 QA Engineers through the initial testing lifecycle of enhancements to a client-facing API",
+      "Facilitate cross-functional communication as the main point of contact for QA on two projects",
+      "Awarded with the Infosys RISE Rookie of the Quarter award in Q4 of 2022",
+    ],
+  },
+  {
+    title: "Software Engineer I - QA",
+    company_name: "Infosys",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "April 2021 - October 2022",
+    points: [
+      "Implemented simeltaneous execution of 20+ automated test cases within a Jenkins pipeline via Selenium Grid",
+      "Executed functional, automated, smoke, and black box testing within a 6-week regression / release cycle",
+      "Prevented unnecessary access to client PII through use of CA TDM to create 4 large test data sets",
+    ],
+  },
+  {
+    title: "Associate Software Developer",
+    company_name: "Revature",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "January 2021 - April 2021",
+    points: [
+      "Created three full-stack web applications including a surveying & data analysis dashboard, an e-commerce website, and a reimbursement management application",
+      "Worked individually and in groups of 3 to 22 engineers to study software engineering principles and build full-stack web applications",
+      "Awarded with the Most Helpful Developer award for assisting in planning and development across the tech stack",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
